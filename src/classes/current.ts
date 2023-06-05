90b1c4d49f7e88d13fdad42835c802c891628276
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-06-05 17:27:29 +0200

    update a few things

--- src/classes/current.ts
+++ src/classes/current.ts
@@ -1,50 +1,48 @@
 import fs from "fs";
-import { Settings } from "../interfaces/settings";
-import { Setting } from "../interfaces/setting";
+import { Settings } from "../interfaces/settings.js";
+import { Setting } from "../interfaces/setting.js";
 
 export default class CurrentSettings {
     settings: Settings;
+    jsonPath: string;
 
-    constructor(jsonPath: string | undefined) {
-        if (jsonPath) {
-            if (fs.existsSync(jsonPath)) {
-                this.settings = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
-            }
-        }
+    constructor(jsonPath: string) {
+        this.settings = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+        this.jsonPath = jsonPath;
     }
 
     enable(id: string) {
         for (const setting of this.settings.disabled) {
             if (setting.id == id) {
                 this.settings.enabled.push(setting);
-                this.settings.disabled.filter((val) => val.id != id);
+                this.settings.disabled.splice(this.settings.disabled.indexOf(setting), 1);
             }
         }
     }
 
 
     disable(id: string) {
-        for (const setting of this.settings.disabled) {
+        for (const setting of this.settings.enabled) {
             if (setting.id == id) {
                 this.settings.disabled.push(setting);
-                this.settings.enabled.filter((val) => val.id != id);
+                this.settings.enabled.splice(this.settings.enabled.indexOf(setting), 1);
             }
         }
     }
 
-    save(path: string) {
-        fs.writeFileSync(path, JSON.stringify(this.settings));
+    save() {
+        fs.writeFileSync(this.jsonPath, JSON.stringify(this.settings, null, 2));
     }
 
-    read(path: string) {
-        this.settings = JSON.parse(fs.readFileSync(path, 'utf8'));
+    read() {
+        this.settings = JSON.parse(fs.readFileSync(this.jsonPath, 'utf8'));
     }
 
-    set(id: string, setTo: string | number) {
+    set(id: string, setTo: string | number | boolean) {
         for (const settingType in this.settings) {
-            for (const setting of this.settings[settingType]) {
+            for (const setting of (settingType == "enabled" ? this.settings.enabled : this.settings.disabled)) {
                 if (setting.id == id) {
-                    this.settings[settingType].forEach((element: Setting) => {
+                    (settingType == "enabled" ? this.settings.enabled : settingType == "disabled" ? this.settings.disabled : this.settings.enabled).forEach((element: Setting) => {
                         if (element.id == id) element.set = setTo;
                     })
                 }
@@ -52,12 +50,20 @@ export default class CurrentSettings {
         }
     }
 
-    addCustom(id: string, set: string | number) {
-        this.settings.custom.push(
+    add(tbl: string, id: string, set: string | number | boolean) {
+        (tbl == "enabled" ? this.settings.enabled : this.settings.disabled).push(
             {
                 id: id,
                 set: set
             }
         )
+    }
+
+    toJSON() {
+        const mapped: { [key: string]: string | number | boolean} = {};
+        for (const item of this.settings.enabled) {
+            mapped[item.id] = item.set;
+        }
+        return JSON.stringify(mapped, null, 2);
     }
 }
