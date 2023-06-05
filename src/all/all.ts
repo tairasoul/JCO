118f81cd4c66b86c5008ef22ed6e4481731ccbbc
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-06-05 17:27:45 +0200

    mover was removed

--- src/all/all.ts
+++ src/all/all.ts
@@ -1,3 +1,2 @@
-export {default as current} from '../classes/current';
-export {default as mover} from '../classes/mover';
-export {default as utils} from '../utils/utils';
+export {default as current} from '../classes/current.js';
+export {default as utils} from '../utils/utils.js';
