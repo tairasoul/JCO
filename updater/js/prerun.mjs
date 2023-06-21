64b0ff97b125379b6ff7a29f0374af969ee880d5
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-06-21 10:28:55 +0200

    Create prerun.mjs

---
+++ updater/js/prerun.mjs
@@ -0,0 +1,12 @@
+import { exec } from 'child_process';
+
+const run = async (cmd) => {
+    const child = exec(cmd, (err) => {
+        if (err) console.error(err);
+    });
+    child.stderr.pipe(process.stderr);
+    child.stdout.pipe(process.stdout);
+    await new Promise((resolve) => child.on('close', resolve));
+};
+
+await run("npm i axios chalk");
