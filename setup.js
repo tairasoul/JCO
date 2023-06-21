91746c2fb6c16d26d23d6401b64ca1cdca687ece
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-06-21 09:16:13 +0200

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const packagejson = require("rfo.js/package.json");
+
+packagejson.type = "module";
+
+fs.writeFileSync(__dirname + "/node_modules/rfo.js/package.json", JSON.stringify(packagejson, null, 4));
+
+const rfo = fs.readFileSync(__dirname + "/node_modules/rfo.js/dist/RFO.js", 'utf8');
+
+const lines = rfo.split("\n");
+
+lines[6] = `
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(decodeURIComponent(fileURLToPath(import.meta.url)));
+`
+
+const res = lines.join('\n');
+
+fs.writeFileSync(__dirname + "/node_modules/rfo.js/dist/RFO.js", res)
