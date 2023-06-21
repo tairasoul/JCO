05d670d170deec88aa31c8fba6d82166eb9df91e
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-06-21 10:29:06 +0200

    Create main.mjs

---
+++ updater/js/main.mjs
@@ -0,0 +1,121 @@
+import { exec, execSync } from 'child_process';
+import { default as axios } from 'axios';
+import fs from 'fs';
+import chalk from 'chalk';
+
+// thanks stackoverflow https://stackoverflow.com/questions/72208532/install-npm-package-programmatically-after-npm-decision-to-drop-programmatic-api
+
+const run = async (cmd) => {
+    const child = exec(cmd, (err) => {
+        if (err) console.error(err);
+    });
+    child.stderr.pipe(process.stderr);
+    child.stdout.pipe(process.stdout);
+    await new Promise((resolve) => child.on('close', resolve));
+};
+
+const test = async (cmd) => {
+    try {
+        execSync(cmd);
+        return true
+    }
+    catch {
+        return false
+    }
+}
+
+const __dirname = "C:/JCO";
+
+const baseURL = "https://raw.githubusercontent.com/fheahdythdr/JCO/main/";
+
+const files = [
+    "src/main.ts",
+    "src/lib/searcher.ts",
+    "src/definitions/index.ts",
+    "src/definitions/interfaces.ts",
+    "tsconfig.json",
+    "package.json"
+]
+
+fs.mkdirSync(`${__dirname}/build/src`, {recursive: true});
+fs.mkdirSync(`${__dirname}/build/src/lib`, {recursive: true});
+fs.mkdirSync(`${__dirname}/build/src/definitions`, {recursive: true})
+
+console.log(chalk.bold.green("Downloading necessary files."))
+for (const file of files) {
+    const data = (await axios.get(baseURL + file)).data;
+    console.log(chalk.bold.green(`Downloading ${file}.`))
+    if (typeof data == "object") fs.writeFileSync(`${__dirname}/build/${file}`, JSON.stringify(data));
+    else fs.writeFileSync(`${__dirname}/build/${file}`, data);
+    console.log(chalk.bold.green(`Downloaded ${file}, waiting 1 second.`))
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+console.log(chalk.bold.blue("Getting package.json data."))
+
+const packagedata = JSON.parse(fs.readFileSync(`${__dirname}/build/package.json`, 'utf8'));
+fs.rmSync(`${__dirname}/build/package.json`)
+
+let toInstall = "";
+
+for (const entry of Object.keys(packagedata.devDependencies)) {
+    toInstall += `${entry} `
+}
+
+for (const entry of Object.keys(packagedata.dependencies)) {
+    if (entry != "rfo.js") toInstall += `${entry} `;
+}
+
+console.log(chalk.bold.blue("Installing all necessary packages."))
+
+await run(`cd ${__dirname}/build && npm install ${toInstall.trim()}`);
+
+await run(`cd ${__dirname}/build && npm install git+https://github.com/rbxflags/js-api`)
+
+if (!test("tsc -v")) await run("npm install tsc")
+
+console.log(chalk.bold.blue("Building from source."))
+
+execSync(`cd ${__dirname}/build && tsc`);
+
+console.log(chalk.bold.blue("Removing unnecessary files."))
+
+fs.rmSync(`${__dirname}/build/src`, {recursive: true});
+fs.rmSync(`${__dirname}/build/node_modules`, {recursive: true});
+fs.rmSync(`${__dirname}/build/package-lock.json`, {recursive: true});
+fs.rmSync(`${__dirname}/build/package.json`, {recursive: true});
+fs.rmSync(`${__dirname}/build/tsconfig.json`, {recursive: true});
+
+console.log(chalk.bold.red("Removing outdated files."))
+
+try {
+    fs.rmSync(`${__dirname}/Main/main.js`, {recursive: true});
+    fs.rmSync(`${__dirname}/Main/main.d.ts`, {recursive: true});
+    fs.rmSync(`${__dirname}/Main/lib/searcher.js`, {recursive: true});
+    fs.rmSync(`${__dirname}/Main/lib/searcher.d.ts`, {recursive: true});
+    fs.rmSync(`${__dirname}/Main/definitions/index.js`, {recursive: true});
+    fs.rmSync(`${__dirname}/Main/definitions/index.d.ts`, {recursive: true});
+    fs.rmSync(`${__dirname}/Main/definitions/interfaces.d.ts`, {recursive: true});
+    fs.rmSync(`${__dirname}/Main/definitions/interfaces.js`, {recursive: true});
+} catch {}
+
+console.log(chalk.bold.red("Moving new files."))
+
+function copy(file) {
+    fs.copyFileSync(`${__dirname}/build/dist/${file}`, `${__dirname}/Main/${file}`);
+}
+
+copy("main.js");
+copy("main.d.ts");
+fs.mkdirSync(`${__dirname}/Main/lib`);
+fs.mkdirSync(`${__dirname}/Main/definitions`);
+copy("lib/searcher.js");
+copy('lib/searcher.d.ts');
+copy("definitions/index.d.ts");
+copy("definitions/index.js");
+copy("definitions/interfaces.d.ts");
+copy("definitions/interfaces.js");
+
+console.log(chalk.bold.green("Moved files."));
+console.log(chalk.bold.green("Removing build dir."));
+fs.rmSync(`${__dirname}/build`, {recursive: true});
